c41fa0928369ffebe62e5d4d826faa9bdfb1c353
Angel Kafazov <akafazov@daiteap.com>
2023-11-14 19:44:35 +0200

    Enable volume control

--- config.h
+++ config.h
@@ -65,6 +65,8 @@ static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray
 static const char *termcmd[]  = { "i3-sensible-terminal", NULL };
 static const char *googlechromecmd[] = { "google-chrome-stable", NULL };
 
+#include <X11/XF86keysym.h>
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -101,6 +103,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+        { 0,                            XF86XK_AudioRaiseVolume,    spawn,      SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%") },
+        { 0,                            XF86XK_AudioLowerVolume,    spawn,      SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%") },
 };
 
 /* button definitions */
